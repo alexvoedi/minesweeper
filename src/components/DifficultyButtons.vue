26b476a78ff2c2a9642345d92561978eaec5757d
Alexander Vödisch <voedisch.alexander@gmail.com>
2021-12-28 21:55:47 +0100

    refactor: difficulty button component

--- src/components/DifficultyButtons.vue
+++ src/components/DifficultyButtons.vue
@@ -2,59 +2,38 @@
 defineEmits([
   'init-game',
 ])
+
+const options = [
+  {
+    title: 'Beginner',
+    settings: { width: 9, height: 9, mines: 10 },
+  },
+  {
+    title: 'Intermediate',
+    settings: { width: 16, height: 16, mines: 40 },
+  },
+  {
+    title: 'Expert',
+    settings: { width: 30, height: 16, mines: 99 },
+  },
+]
 </script>
 
 <template>
   <div class="grid grid-cols-3 gap-5">
-        <button
-          class="
-            btn btn-primary
-            transform
-            hover:shadow-md
-            btn-block
-            block
-            h-auto
-            py-3
-          "
-          @click="$emit('init-game', { width: 9, height: 9, mines: 10 })"
-        >
-          <div>Beginner</div>
-          <div class="font-light normal-case">9x9 - 10 Mines</div>
-        </button>
+    <button
+      v-for="{ title, settings } in options"
+      :key="title"
+      class="btn btn-primary transform hover:shadow-md btn-block block h-auto py-3"
+      @click="$emit('init-game', settings)"
+    >
+      <div>{{ title }}</div>
+      <div
+        class="font-light normal-case"
+      >{{ settings.width }}x{{ settings.height }} - {{ settings.mines }} Mines</div>
+    </button>
 
-        <button
-          class="
-            btn btn-primary
-            transform
-            hover:shadow-md
-            btn-block
-            block
-            h-auto
-            py-3
-          "
-          @click="$emit('init-game', { width: 16, height: 16, mines: 40 })"
-        >
-          <div>Intermediate</div>
-          <div class="font-light normal-case">16x16 - 40 Mines</div>
-        </button>
-
-        <button
-          class="
-            btn btn-primary
-            transform
-            hover:shadow-md
-            btn-block
-            block
-            h-auto
-            py-3
-          "
-          @click="$emit('init-game', { width: 30, height: 16, mines: 99 })"
-        >
-          <div>Expert</div>
-          <div class="font-light normal-case">30x16 - 99 Mines</div>
-        </button>
-
-        <!-- <button
+    <!-- <button
           class="
             btn btn-secondary
             transform
@@ -68,10 +47,9 @@ defineEmits([
         >
           <div>Custom</div>
           <div class="font-light normal-case">Define your own game</div>
-        </button> -->
-      </div>
+    </button>-->
+  </div>
 </template>
 
 <style>
-
 </style>
